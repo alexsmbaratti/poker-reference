b9fb9ede79ca1027d02b20fc65ff85b85029a325
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2022-12-11 17:31:27 -0800

    Uses ForEach to create CardStackView

--- Poker Face/QuickReferenceView.swift
+++ Poker Face/QuickReferenceView.swift
@@ -15,13 +15,13 @@ struct QuickReferenceView: View {
             ScrollView {
                 HandReferenceView(name: "Royal Flush", description: "Five cards of the same suit in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Straight Flush", description: "Five cards of the same suit in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
+                HandReferenceView(name: "Straight Flush", description: "Five cards of the same suit in sequence", cards: [Card(rank: .nine, suit: .spade), Card(rank: .eight, suit: .spade), Card(rank: .seven, suit: .spade), Card(rank: .six, suit: .spade), Card(rank: .five, suit: .spade)])
                 Divider()
-                HandReferenceView(name: "4 Of A Kind", description: "Four cards of the same rank, plus an unmatched card", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
+                HandReferenceView(name: "4 Of A Kind", description: "Four cards of the same rank, plus an unmatched card", cards: [Card(rank: .seven, suit: .club), Card(rank: .seven, suit: .diamond), Card(rank: .seven, suit: .heart), Card(rank: .seven, suit: .spade), Card(rank: .two, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Full House", description: "Three matching cards of one rank, with two matching cards of another", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
+                HandReferenceView(name: "Full House", description: "Three matching cards of one rank, with two matching cards of another", cards: [Card(rank: .queen, suit: .heart), Card(rank: .queen, suit: .spade), Card(rank: .queen, suit: .diamond), Card(rank: .ace, suit: .club), Card(rank: .ace, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Flush", description: "Five cards of the same suit, but not in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
+                HandReferenceView(name: "Flush", description: "Five cards of the same suit, but not in sequence", cards: [Card(rank: .king, suit: .club), Card(rank: .seven, suit: .club), Card(rank: .five, suit: .club), Card(rank: .three, suit: .club), Card(rank: .two, suit: .club)])
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing, content: {
@@ -62,25 +62,22 @@ struct HandReferenceView: View {
                     }
                 }
                 .multilineTextAlignment(.leading)
-                CardStack()
+                CardStackView(cards: cards)
             }
             Spacer()
         }
     }
 }
 
-struct CardStack: View {
+struct CardStackView: View {
+    var cards: [Card]
+    
     var body: some View {
         ZStack {
-            CardView(card: Card(rank: .ace, suit: .heart))
-            CardView(card: Card(rank: .king, suit: .heart))
-                .offset(x: 25)
-            CardView(card: Card(rank: .queen, suit: .heart))
-                .offset(x: 50)
-            CardView(card: Card(rank: .jack, suit: .heart))
-                .offset(x: 75)
-            CardView(card: Card(rank: .ten, suit: .heart))
-                .offset(x: 100)
+            ForEach(0..<cards.count, id: \.self) { index in
+                CardView(card: cards[index])
+                    .offset(x: CGFloat(25 * index))
+            }
         }
         .offset(x: -50)
         .frame(width: 200, height: 125)
@@ -99,7 +96,7 @@ struct CardView: View {
                 VStack {
                     Text(card.rank.symbol)
                         .fontWeight(.bold)
-                    .foregroundColor(.black)
+                        .foregroundColor(.black)
                     Image(systemName: card.suit.symbol)
                         .foregroundColor(card.suit.color)
                     Spacer()

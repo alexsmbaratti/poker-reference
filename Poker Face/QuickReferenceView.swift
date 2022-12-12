fe9a9b338f0ddcbc6c7590870970979f2c613fda
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2022-12-11 17:22:53 -0800

    Adjusts frame for HandReferenceView

--- Poker Face/QuickReferenceView.swift
+++ Poker Face/QuickReferenceView.swift
@@ -13,15 +13,15 @@ struct QuickReferenceView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                HandReferenceView(name: "Royal Flush", description: "Five cards of the same suit in sequence")
+                HandReferenceView(name: "Royal Flush", description: "Five cards of the same suit in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Straight Flush", description: "Five cards of the same suit in sequence")
+                HandReferenceView(name: "Straight Flush", description: "Five cards of the same suit in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "4 Of A Kind", description: "Four cards of the same rank, plus an unmatched card")
+                HandReferenceView(name: "4 Of A Kind", description: "Four cards of the same rank, plus an unmatched card", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Full House", description: "Three matching cards of one rank, with two matching cards of another")
+                HandReferenceView(name: "Full House", description: "Three matching cards of one rank, with two matching cards of another", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
                 Divider()
-                HandReferenceView(name: "Flush", description: "Five cards of the same suit, but not in sequence")
+                HandReferenceView(name: "Flush", description: "Five cards of the same suit, but not in sequence", cards: [Card(rank: .ace, suit: .heart), Card(rank: .king, suit: .heart), Card(rank: .queen, suit: .heart), Card(rank: .jack, suit: .heart), Card(rank: .ten, suit: .heart)])
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing, content: {
@@ -42,6 +42,7 @@ struct QuickReferenceView: View {
 struct HandReferenceView: View {
     var name: String
     var description: String
+    var cards: [Card]
     
     var body: some View {
         VStack {
@@ -61,21 +62,28 @@ struct HandReferenceView: View {
                     }
                 }
                 .multilineTextAlignment(.leading)
-                ZStack {
-                    CardView(card: Card(rank: .ace, suit: .heart))
-                    CardView(card: Card(rank: .king, suit: .heart))
-                        .offset(x: 25)
-                    CardView(card: Card(rank: .queen, suit: .heart))
-                        .offset(x: 50)
-                    CardView(card: Card(rank: .jack, suit: .heart))
-                        .offset(x: 75)
-                    CardView(card: Card(rank: .ten, suit: .heart))
-                        .offset(x: 100)
-                }
-                .frame(width: 270)
+                CardStack()
             }
             Spacer()
         }
+    }
+}
+
+struct CardStack: View {
+    var body: some View {
+        ZStack {
+            CardView(card: Card(rank: .ace, suit: .heart))
+            CardView(card: Card(rank: .king, suit: .heart))
+                .offset(x: 25)
+            CardView(card: Card(rank: .queen, suit: .heart))
+                .offset(x: 50)
+            CardView(card: Card(rank: .jack, suit: .heart))
+                .offset(x: 75)
+            CardView(card: Card(rank: .ten, suit: .heart))
+                .offset(x: 100)
+        }
+        .offset(x: -50)
+        .frame(width: 200, height: 125)
     }
 }
 

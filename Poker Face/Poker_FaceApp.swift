4c9d305f92c005a2e75be43724d1b6c745afe7b5
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2024-01-10 19:53:48 -0800

    Set minimum frame in visionOS

--- Poker Face/Poker_FaceApp.swift
+++ Poker Face/Poker_FaceApp.swift
@@ -12,6 +12,17 @@ struct Poker_FaceApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+#if os(visionOS)
+                .frame(minWidth: 850, minHeight: 850)
+#endif
         }
+#if os(visionOS)
+        .windowResizability(.contentSize)
+#endif
+#if os(visionOS)
+        WindowGroup(id: "quick-reference") {
+            QuickReferenceView(isShowing: .constant(true))
+        }
+#endif
     }
 }

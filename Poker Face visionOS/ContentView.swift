a4eaa056525df83c7bcdca393224a141dc853921
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-03-02 11:19:56 -0800

    visionOS in its own target

---
+++ Poker Face visionOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Poker Face visionOS
+//
+//  Created by Alex Baratti on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Poker Face visionOS/Poker_Face_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_Face_visionOSApp.swift
+//  Poker Face visionOS
+//
+//  Created by Alex Baratti on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Poker_Face_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

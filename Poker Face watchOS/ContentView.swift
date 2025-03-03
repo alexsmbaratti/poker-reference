9f0edb82ead78e52864054a80234ca30233b60f6
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-03-02 19:08:10 -0800

    Add TinyCardView

--- Poker Face watchOS/ContentView.swift
+++ Poker Face watchOS/ContentView.swift
@@ -19,7 +19,7 @@ struct HandsView: View {
             VStack(spacing: 16) {
                 ForEach(Hand.allCases, id: \.rawValue) { hand in
                     VStack {
-                        CardStackView(cards: hand.exampleHand) // TODO: Fit screen size
+                        TinyCardStackView(cards: hand.exampleHand)
                         Text(hand.name)
                             .font(.headline)
                             .foregroundStyle(.gray)
@@ -28,6 +28,55 @@ struct HandsView: View {
                 }
             }
             .padding(.vertical)
+        }
+    }
+}
+
+struct TinyCardStackView: View {
+    var cards: [Card]
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<cards.count, id: \.self) { index in
+                TinyCardView(card: cards[index])
+                    .offset(x: CGFloat(25 * index))
+            }
+        }
+        .offset(x: -50)
+        .frame(width: CGFloat(cards.count) * 40, height: 125)
+    }
+}
+
+struct TinyCardView: View {
+    var card: Card
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .foregroundStyle(card.isFaceUp ? .white : .gray)
+                .shadow(radius: 5)
+            if card.isFaceUp {
+                HStack {
+                    VStack {
+                        label
+                        Spacer()
+                    }
+                    .dynamicTypeSize(.medium)
+                    Spacer()
+                }
+                .padding(.all, 3.0)
+            }
+        }
+        .frame(width: 50, height: 70)
+    }
+    
+    var label: some View {
+        VStack {
+            Text(card.rank.symbol)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            Image(systemName: card.suit.symbol)
+                .foregroundColor(card.suit.color)
         }
     }
 }
